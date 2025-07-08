895bce0d230b0e78bf2ef0cec77a7a1c80a5bfd4
Mounesh v <mouneshv696@gmail.com>
2025-07-08 12:54:00 +0530

    Updated frontend components or features

--- src/Courses/SkillsShowcase.jsx
+++ src/Courses/SkillsShowcase.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import Particles from "react-tsparticles";
+import Particles from "react-tsparticles"; 
 import { loadFull } from "tsparticles";
 
 gsap.registerPlugin(ScrollTrigger);
